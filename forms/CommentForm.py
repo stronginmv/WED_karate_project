200264b15ec46a5e7f0227e638932fb29708af15
stronginmv <94210424+stronginmv@users.noreply.github.com>
2022-05-07 15:34:03 +1000

    Delete forms directory

--- forms/CommentForm.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import TextAreaField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class ComForm(FlaskForm):
-    content = TextAreaField('Что вы хотели сказать?', validators=[DataRequired()])
-    submit = SubmitField('Оставить комментарий')
